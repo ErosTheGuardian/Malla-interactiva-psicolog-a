686b18cc422b6ec01912116e256f8eab0ade6a74
ErosTheGuardian <d13g0rg2004@gmail.com>
2025-07-13 21:23:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+// Este archivo contiene la lógica JS para la malla interactiva
+
+// Por claridad, el contenido JS ha sido embebido directamente en index.html para esta primera versión.
+// Si deseas, puedo trasladarlo a este archivo y conectarlo desde index.html para modular mejor tu código.
 
